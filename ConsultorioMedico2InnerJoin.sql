26dc76c98f8899e786f1cd08655736709a50bd23
EDUARDO VINICIUS FALICO DA COSTA <eduardo.vfcosta@senacsp.edu.br>
2024-11-07 21:54:32 -0300

    ConsultorioMedico2InnerJoin

--- ConsultorioMedico2InnerJoin.sql
+++ ConsultorioMedico2InnerJoin.sql
@@ -1,9 +1,6 @@
-
-
 USE MASTER
 
-
-CREATE DATABASE ConsultorioMedico;
+CREATE DATABASE ConsultorioMedico
 
 USE ConsultorioMedico
 GO
@@ -55,16 +52,25 @@ INSERT INTO Paciente VALUES ('Eduardo Falico','11/11/1996','Rua das Flores n 40 
 
 
 
-INSERT INTO Medico VALUES('Dr Jose Ricardo',25665, 1),
+INSERT INTO Medico VALUES('Dr Jose Ricardo',25665,1),
 						('Dra Alessandra Maia',25655,2)
+
 
 
 INSERT INTO Especialidade VALUES('Clinico Geral'),
 								('Pediatria')
 
 
+
 INSERT INTO Consulta VALUES (2,1,'12/12/2024','Exames de sangue anexado em portifolios'),
 					        (1,2,'11/25/2024','Exame de urina anexado')
+
+
+
+UPDATE Medico
+SET NomeMedico= 'Dr Jose Flavio'
+WHERE IdMedico = 1;
+
 
 
 select * from Paciente		   
@@ -76,13 +82,19 @@ DELETE FROM Especialidade WHERE IdEspecialidade=3
 DELETE FROM Especialidade WHERE IdEspecialidade=4
 
 
-SELECT Paciente.IdPaciente, Paciente.Endereco,Paciente.Nome,Medico.Nome
+EXEC sp_rename  'Paciente.Nome', 'NomePaciente', 'COLUMN';
+EXEC sp_rename  'Medico.Nome', 'NomeMedico', 'COLUMN';
+
+
+SELECT Paciente.IdPaciente as 'Id Paciente', 
+Paciente.Endereco as 'Endereço',
+Paciente.NomePaciente as 'Nome Paciente',
+Medico.NomeMedico as 'Nome Medico',
+Consulta.DataConsulta as 'Data da Consulta'
 FROM Consulta
 INNER JOIN Medico ON Medico.IdEspecialidade = Consulta.IdMedico
 INNER JOIN Paciente ON Paciente.IdPaciente = Consulta.IdPaciente
-
-
-
+WHERE DataConsulta BETWEEN '12-12-2023' AND '12-25-2024'
 
 
 BACKUP DATABASE LojaInformaticaDB 
